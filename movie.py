65473493e946b15010b2a0bbe5ca8d83f487e1ac
Diogo V. Kersting <diogovk@gmail.com>
2015-11-29 23:27:39 -0200

    improve documentation for Movie

--- movie.py
+++ movie.py
@@ -1,7 +1,15 @@
 
-class Movie:
-    """Represents a movie, containing relevant information, such as title,
-       trailer url, etc"""
+class Movie(object):
+    """A movie, containing information such as title, trailer url, etc
+
+    Keyword Arguments:
+        - title -- title of the movie
+
+        - trailer_youtube_url -- youtube URL containing the trailer of movie
+
+        - poster_image_url -- URL pointing to a poster of the movie
+   """
+
     def __init__(self, title, trailer_youtube_url, poster_image_url):
         self.title = title
         self.trailer_youtube_url = trailer_youtube_url
